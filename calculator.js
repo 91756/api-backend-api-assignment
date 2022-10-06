2a4ab6172bcb08674e1d7519284dbcc0615d0b2d
krishna <krishna28@gmail.com>
2022-10-06 02:14:02 +0000

    node backend api

---
+++ calculator.js
@@ -0,0 +1,13 @@
+const mul = function (a, b) {
+  return a * b;
+};
+
+const add = function (a, b) {
+  return a + b;
+};
+const sub = function (a, b) {
+  return a - b;
+};
+exports.add = add;
+exports.mul = mul;
+exports.sub = sub;
